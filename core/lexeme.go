58bbf13a249d9656b4dffb4dfc29194719601557
Anton Kolesov <kolesov.anton.s@gmail.com>
2024-02-15 01:01:12 +0300

    Added lexer implementation

--- core/lexeme.go
+++ core/lexeme.go
@@ -7,15 +7,15 @@ type Lexeme struct {
 	value         string
 }
 
-type LexemeType int
+type LexemeType string
 
 const (
-	NL LexemeType = iota
-	AT_CHR
-	DASH_CHR
-	HASH_CHR
-	WORD
-	SPACE
-	FEATURE_WORD
-	SCENARIO_WORD
+	NL            LexemeType = "NL"
+	AT_CHR        LexemeType = "AT_CHR"
+	DASH_CHR      LexemeType = "DASH_CHR"
+	HASH_CHR      LexemeType = "HASH_CHR"
+	WORD          LexemeType = "WORD"
+	SPACE         LexemeType = "SPACE"
+	FEATURE_WORD  LexemeType = "FEATURE_WORD"
+	SCENARIO_WORD LexemeType = "SCENARIO_WORD"
 )
---
+++ core/lexer_test.go
@@ -0,0 +1,39 @@
+package core
+
+import (
+	"fmt"
+	"testing"
+)
+
+const code string = `
+@all
+Feature: Basic calculator operations
+
+This feature defines a set of operations that the calculator must support.
+
+# Comment example
+
+@pass @automated
+Scenario: Addition
+- Enter "2 + 2"
+- Click on calculation button
+- Check that the answer is "5"
+
+@fail
+Scenario: Division by zero
+- Enter "5 / 0"
+- Click on calculation button
+- Аn exception must be thrown
+
+`
+
+func TestLexer(t *testing.T) {
+
+	lexer := NewLexer(code)
+	lexemes := lexer.Scan()
+
+	for _, lexeme := range lexemes {
+		fmt.Println(lexeme)
+	}
+
+}
