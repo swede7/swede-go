df260c0ea90fcffd6f9102db72bb58a0f659af49
Anton Kolesov <kolesov.anton.s@gmail.com>
2024-02-21 23:39:18 +0300

    Changed format command description

--- cmd/format.go
+++ cmd/format.go
@@ -17,13 +17,7 @@ import (
 // formatCmd represents the format command
 var formatCmd = &cobra.Command{
 	Use:   "format",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Format one or more files",
 	Run: func(cmd *cobra.Command, args []string) {
 		formatFilesParallel(args)
 	},
