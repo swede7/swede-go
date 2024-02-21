c9a64c6fc06064d3fd27e7d63ae37fbad364a896
Anton Kolesov <kolesov.anton.s@gmail.com>
2024-02-21 23:36:58 +0300

    Added format command to cli, move exmaples to new directory

--- cmd/format.go
+++ cmd/format.go
@@ -4,9 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"me.weldnor/swede/core/formatter"
+	"me.weldnor/swede/core/lexer"
+	"me.weldnor/swede/core/parser"
 )
 
 // formatCmd represents the format command
@@ -20,20 +25,55 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("format called")
+		formatFilesParallel(args)
 	},
+}
+
+func formatFilesParallel(paths []string) {
+	statusChan := make(chan string)
+
+	for _, path := range paths {
+		go func() {
+			err := formatFile(path)
+
+			if err != nil {
+				statusChan <- "can`t format file" + path
+
+			} else {
+				statusChan <- "file " + path + " formatted successfully"
+			}
+		}()
+	}
+
+	for range len(paths) {
+		var status string = <-statusChan
+		fmt.Println(status)
+	}
+
+}
+
+func formatFile(path string) error {
+	code, err := os.ReadFile(path)
+
+	lexer := lexer.NewLexer(string(code))
+	parser := parser.NewParser(lexer.Scan())
+	parserResult := parser.Parse()
+
+	if len(parserResult.Errors) > 0 {
+		return errors.New("found errors while parsing file: " + path)
+	}
+
+	_formatter := formatter.NewFormatter(&parserResult.RootNode)
+	formattedCode, err := _formatter.FormatParallel()
+
+	if err != nil {
+		return err
+	}
+
+	os.WriteFile(path, []byte(formattedCode), 0644)
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(formatCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// formatCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// formatCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
