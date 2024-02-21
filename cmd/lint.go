cad97c22b348fcbac42ed951a2cfed218cb04dbc
Anton Kolesov <kolesov.anton.s@gmail.com>
2024-02-22 00:23:48 +0300

    Added lint command to cli.

--- cmd/lint.go
+++ cmd/lint.go
@@ -4,36 +4,82 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"sync"
 
 	"github.com/spf13/cobra"
+	"me.weldnor/swede/core/lexer"
+	"me.weldnor/swede/core/linter"
+	"me.weldnor/swede/core/parser"
 )
 
 // lintCmd represents the lint command
 var lintCmd = &cobra.Command{
 	Use:   "lint",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Lint one or more files",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("lint called")
+		lintFilesParallel(args)
 	},
+}
+
+func lintFilesParallel(paths []string) {
+	wg := &sync.WaitGroup{}
+	mutex := sync.Mutex{}
+
+	for _, path := range paths {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			linterErrors, err := lintFile(path, mutex)
+
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			fmt.Println("processing " + path + "...")
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			if len(linterErrors) == 0 {
+				fmt.Println("no errors found. GOOD")
+				return
+			}
+
+			for _, linterError := range linterErrors {
+				fmt.Printf("%s [%d:%d] %s\n", linterError.Severity, linterError.StartPosition.Line+1, linterError.StartPosition.Column+1, linterError.Message)
+			}
+
+		}()
+	}
+
+	wg.Wait()
+}
+
+func lintFile(path string, mutex sync.Mutex) ([]linter.LinterError, error) {
+	code, err := os.ReadFile(path)
+
+	if err != nil {
+		return nil, errors.New("cant read file" + path)
+	}
+
+	lexer := lexer.NewLexer(string(code))
+	parser := parser.NewParser(lexer.Scan())
+	parserResult := parser.Parse()
+
+	if len(parserResult.Errors) > 0 {
+		return nil, errors.New("found errors while parsing file: " + path)
+	}
+
+	linter := linter.NewLinter(&parserResult.RootNode)
+	return linter.Lint(), nil
 }
 
 func init() {
 	rootCmd.AddCommand(lintCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lintCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lintCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
